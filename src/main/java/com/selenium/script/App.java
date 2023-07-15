6c3290fb638e24d4b4504555aa391d99a8786ee5
Mohan <mohan.devops019@gmail.com>
2023-07-15 02:31:37 -0400

    Created win.jar for windows and normal for linux

--- src/main/java/com/selenium/script/App.java
+++ src/main/java/com/selenium/script/App.java
@@ -6,10 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
