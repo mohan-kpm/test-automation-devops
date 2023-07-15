f7897da5a89994ff7b7ff7e04e3ce28fd0fb5206
Mohan <mohan.devops019@gmail.com>
2023-07-14 22:54:58 -0400

    first commit

---
+++ src/main/java/com/selenium/script/App.java
@@ -0,0 +1,37 @@
+package com.selenium.script;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // System.out.println( "Hello World!" );
+        // 1. Initialize web driver
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\mohan\\devops\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        // 2. Open the browser
+        driver.get("https://www.facebook.com/"); 
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        
+        driver.findElement(By.id("email")).sendKeys("abc@def.com");
+        driver.findElement(By.id("pass")).sendKeys("dummypassword");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.className("_9lsa")).click();
+        //driver.findElement(By.name("login")).click();
+
+
+
+    }
+}
